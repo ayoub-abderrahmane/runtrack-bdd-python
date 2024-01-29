d8c928d2245391296ffd33504a1688881dec3819
Ayoub Abderrahmane <ayoub-abderrahmane@laplateforme.io>
2024-01-29 17:27:22 +0100

    ajout job 05

---
+++ jour01/job05.sql
@@ -0,0 +1,23 @@
+mysql> INSERT INTO etudiant (nom ,prenom ,age ,email)
+    -> VALUES
+    -> ('Binkie' ,'Barnes', 16 , 'binkie.barnes@laplateforme.io');
+
+mysql> INSERT INTO etudiant (nom ,prenom ,age ,email)
+    -> VALUES
+    -> ('Chuck' ,'Steak', 45 , 'chuck.steak@laplateforme.io');
+
+
+mysql> INSERT INTO etudiant (nom ,prenom ,age ,email)
+    -> VALUES
+    -> ('John' ,'Doe', 18 , 'john.doe@laplateforme.io');
+
+mysql> INSERT INTO etudiant (nom ,prenom ,age ,email)
+    -> VALUES
+    -> ('Gertrude' ,'Dupuis', 20 , 'gertrude.dupuis@laplateforme.io');
+
+mysql> INSERT INTO etudiant (nom ,prenom ,age ,email)
+    -> VALUES
+    -> ('Betty', 'Spaghetti', 23, 'betty.Spaghetti@laplateforme.io');
+
+
+mysql> SELECT * FROM etudiant;
